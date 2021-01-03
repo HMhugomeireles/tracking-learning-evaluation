d0f437121b669bc489bdd67e217bc0f348df2c84
Hugo Meireles <meireles.hmm@gmail.com>
2021-01-03 18:54:07 +0000

    create basic example of set object

---
+++ Javascript/Array/Set/index.js
@@ -0,0 +1,68 @@
+/**
+ * Docs : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+ */
+
+const arrayOfElements = [1,1,2,3,4,2,4,5,2,5,6,8]
+const arrayPersonCities = [
+    {
+        name: "John",
+        age: 22,
+        country: "Portugal",
+        city: "Porto",
+        job: "Design"
+    },
+    {
+        name: "John",
+        age: 33,
+        country: "Portugal",
+        city: "Lisbon",
+        job: "Design"
+    },
+    {
+        name: "John",
+        age: 33,
+        country: "Portugal",
+        city: "Lisbon",
+        job: "Developer"
+    },
+    {
+        name: "John",
+        age: 35,
+        country: "Portugal",
+        city: "Porto",
+        job: "Developer"
+    }
+]
+
+
+/**
+ * 
+ * Basic examples
+ * 
+ */
+function basic() {
+    const setNumbers = new Set(arrayOfElements)
+    console.log("Example with numbers", setNumbers)
+
+    const setOfPersonLiveInPorto = new Set(arrayPersonCities.filter(person => person.city === "Porto"))
+    const setOfPersonLiveInLisbon = new Set(arrayPersonCities.filter(person => person.city === "Lisbon"))
+
+    console.log("Person live in Porto", setOfPersonLiveInPorto)
+    console.log("Person live in Lisbon", setOfPersonLiveInLisbon)
+
+    // Unique values
+    const arrayCities = new Set(arrayPersonCities.map(person => person.city))
+    console.log("Cities ", arrayCities) 
+}
+
+
+/**
+ * 
+ * My examples of Set
+ * 
+ */
+function main() {
+    basic()
+}
+
+main()
