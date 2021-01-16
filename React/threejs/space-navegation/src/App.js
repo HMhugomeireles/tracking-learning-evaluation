947907330cae17b2f45b0b701c5d9cea28217f29
Hugo Meireles <meireles.hmm@gmail.com>
2021-01-16 16:55:08 +0000

    threejs-space-navegation: testing load file

--- React/threejs/space-navegation/src/App.js
+++ React/threejs/space-navegation/src/App.js
@@ -1,9 +1,78 @@
+import { Suspense, useRef } from "react";
+import {
+  Canvas,
+  useLoader,
+  useFrame,
+  extend,
+  useThree
+} from "react-three-fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+function Loading() {
+  return (
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+      <meshStandardMaterial
+        attach="material"
+        color="white"
+        transparent
+        opacity={0.6}
+        roughness={1}
+        metalness={0}
+      />
+    </mesh>
+  );
+}
+
+function ArWing() {
+  const group = useRef();
+  const { nodes } = useLoader(GLTFLoader, "models/arwing.glb");
+  return (
+    <group ref={group}>
+      <mesh visible geometry={nodes.Default.geometry}>
+        <meshStandardMaterial
+          attach="material"
+          color="white"
+          roughness={0.3}
+          metalness={0.3}
+        />
+      </mesh>
+    </group>
+  );
+}
+
+const CameraControls = () => {
+  const {
+    camera,
+    gl: { domElement }
+  } = useThree();
+
+  // Ref to the controls, so that we can update them on every frame using useFrame
+  const controls = useRef();
+  useFrame(state => controls.current.update());
+  return (
+    <orbitControls
+      ref={controls}
+      args={[camera, domElement]}
+      enableZoom={false}
+      maxAzimuthAngle={Math.PI / 4}
+      maxPolarAngle={Math.PI}
+      minAzimuthAngle={-Math.PI / 4}
+      minPolarAngle={0}
+    />
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Canvas style={{ background: "#171717" }}>
+      <CameraControls />
+      <directionalLight intensity={0.5} />
+      <Suspense fallback={<Loading />}>
+        <ArWing />
+      </Suspense>
+    </Canvas>
   );
 }
 
