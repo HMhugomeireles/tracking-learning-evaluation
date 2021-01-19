c10d44768b4c6e017e797dad050d244c56c26f72
Hugo Meireles <meireles.hmm@gmail.com>
2021-01-19 21:34:20 +0000

    testing-practice: make configs for ts and jest

---
+++ Javascript/Testing/practice/babel.config.js
@@ -0,0 +1,20 @@
+module.exports = api => {
+    const isTest = api.env('test');
+    // You can use isTest to determine what presets and plugins to use.
+ 
+    return {
+        presets: [
+            [
+                '@babel/preset-env',
+                '@babel/preset-typescript',
+                {
+                    targets: {
+                        node: 'current',
+                    },
+                },
+            ],
+        ],
+    };
+ 
+};
+
---
+++ Javascript/Testing/practice/jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: [
+    "/node_modules"
+  ],
+  verbose: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
