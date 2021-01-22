7119f33834157ab33f0cd6eae006593ba786de3a
Hugo Meireles <meireles.hmm@gmail.com>
2021-01-22 23:05:21 +0000

    WIP-storybook

--- React/storybook/tutorial/taskbox/src/components/Task.js
+++ React/storybook/tutorial/taskbox/src/components/Task.js
@@ -26,3 +26,20 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
       </div>
     );
   }
+
+  
+Task.propTypes = {
+    /** Composition of the task */
+    task: PropTypes.shape({
+      /** Id of the task */
+      id: PropTypes.string.isRequired,
+      /** Title of the task */
+      title: PropTypes.string.isRequired,
+      /** Current state of the task */
+      state: PropTypes.string.isRequired,
+    }),
+    /** Event to change the task to archived */
+    onArchiveTask: PropTypes.func,
+    /** Event to change the task to pinned */
+    onPinTask: PropTypes.func,
+};
